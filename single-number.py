0bfb9e579684beb83504d06882d247b132cb3531
Zakia Al Kadri <alkadrizakia14@gmail.com>
2025-12-29 22:17:40 -0600

    Create single-number.py

---
+++ single-number.py
@@ -0,0 +1,20 @@
+'''
+Time complexity: O(n)
+Space complexity: O(n) (because of the dictionary)
+'''
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        d = {}
+        for i in nums:
+            if i in d:
+                d[i] = d[i] + 1
+            else:
+                d[i] = 1
+
+        for key in d:
+            if d[key] == 1:
+                return key
+    
+
+        
